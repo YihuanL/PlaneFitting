ec7b82e33bdb95698fb9016542ef232eebf3d09d
ToddQi-HUST <45621782+Qtodd@users.noreply.github.com>
2019-08-23 14:56:33 +0800

    Update test.cpp: Add dirver code to test

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
-#include"GRANSAC.hpp"
-#include"PlaneModel.hpp"
+#include <iostream>
+#include "GRANSAC.hpp"
+#include "PlaneModel.hpp"
 #include <omp.h>
+#include <opencv2/opencv.hpp>
 
-bool PlaneFitting(std::vector<Vector3VP> points_input,int Num double* center, double* normal)
+bool PlaneFitting(const std::vector<Vector3VP> &points_input, double* center, double* normal)
 {
-	
 	int Num = points_input.size();
 	std::vector<std::shared_ptr<GRANSAC::AbstractParameter>> CandPoints;
 	CandPoints.resize(Num);
@@ -17,10 +17,13 @@ bool PlaneFitting(std::vector<Vector3VP> points_input,int Num double* center, do
 		CandPoints[i]=CandPt;
 	}
 	
-
 	GRANSAC::RANSAC<PlaneModel, 3> Estimator;
-	Estimator.Initialize(10, 300); // Threshold, iterations
+    Estimator.Initialize(0.1, 100); // Threshold, iterations
+
+    int64_t start = cv::getTickCount();
 	Estimator.Estimate(CandPoints);
+    int64_t end = cv::getTickCount();
+    std::cout << "RANSAC took: " << GRANSAC::VPFloat(end - start) / GRANSAC::VPFloat(cv::getTickFrequency()) * 1000.0 << " ms." << std::endl;
 	
 	auto BestPlane = Estimator.GetBestModel();
 	if (BestPlane == nullptr)
@@ -29,14 +32,49 @@ bool PlaneFitting(std::vector<Vector3VP> points_input,int Num double* center, do
 	}
 	for (int i = 0; i < 3; i++)
 	{
-		normal[i] = BestPlane->m_PlaneCoefs[i];
-		center[i] = BestPlane->m_PointCenter[i];
+        center[i] = BestPlane->m_PointCenter[i];
 	}
+    for (int i = 0; i < 4; i++)
+    {
+        normal[i] = BestPlane->m_PlaneCoefs[i];
+    }
+
 	return true;
 }
 int main()
 {
-	
+    // data generation
+    double a = -1/3.0, b = -2/3.0, d =3.0;
+    cv::RNG rng;
+    double w_sigma = 0.1;
+
+    std::vector<Vector3VP> point_cloud;
+    double *center = new double[3];
+    double *coefs = new double[4];
+
+    for(int i=0; i<300; i++)
+    {
+        for(int j=0; j<400; j++)
+        {
+            double x = double(i);
+            double y = double(j);
+            double z = a*x + b*y + d + rng.gaussian(w_sigma);
+
+            Vector3VP Pt3d = {x, y, z};
+
+            point_cloud.push_back(Pt3d);
+        }
+    }
+
+    // perform Plane Fitting Algorithm
+    PlaneFitting(point_cloud, center, coefs);
+
+    for(int i=0; i<4; i++)
+    {
+        std::cout << coefs[i] << std::endl;
+    }
+
+    return 0;
 }
 
 
