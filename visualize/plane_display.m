66c33d5c20cba729cc8bb403fe41716f0fc52830
ToddQi-HUST <45621782+Qtodd@users.noreply.github.com>
2019-08-23 17:38:47 +0800

    Add files via upload

--- visualize/plane_display.m
+++ visualize/plane_display.m
@@ -1,21 +1,19 @@
 close all
 clear all
+
+% Plane Equation
 % z = ax+by+d
-% a = -1/3; % -1/3.0 in CPP
-% b = -2/3;
-% d = 3;
-a = 100;
-b = 100;
-d = 1;
+a = -1/3;
+b = -2/3;
+d = 3;
 
 x = 1:1:100;
 y = 1:1:100;
-
 [x, y] = meshgrid(x, y);
 z = a*x+b*y+d;
-%z2 = 2*a*x+2*b*y+d;
+
 figure, surf(x, y, z); hold on
-%surf(x, y, z2)
+
 title('plane display')
 x1 = xlabel('x axis');
 y1 = ylabel('y axis');
